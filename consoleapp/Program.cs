4d276e0583aaffe878f7f4bc054c4eaec1315cea
Oscar Roman <oscarroman.pe@gmail.com>
2023-08-20 17:44:38 -0500

    Course finished

---
+++ consoleapp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Humanizer;
+
+Console.Write("Por favor, ingrese un nombre: ");
+var nombre = Console.ReadLine();
+Console.Write("Por favor, ingrese su cargo: ");
+var cargo = Console.ReadLine();
+Console.Write("Por favor, ingrese su edad: ");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hola, mi nombre es {nombre}, trabajo como {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años.");
